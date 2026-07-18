de85f596135d6416f37edcfe74f94e5ce3ca8249
gaurakshay1991 <gaur.akshay1991@gmail.com>
2026-07-19 05:03:53 +0530

    Run one-time bootstrap admin synchronization on Vercel

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
-import app from '../server/src/app.js';
+import { synchronizeBootstrapAdmin } from '../server/src/bootstrap-admin.js';
+
+await synchronizeBootstrapAdmin();
+const { default: app } = await import('../server/src/app.js');
 
 export default app;
