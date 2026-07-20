605dd39197e7a6fdba019c5f172d3f8914bc77c7
gaurakshay1991 <gaur.akshay1991@gmail.com>
2026-07-20 12:50:31 +0530

    Load unified institutional interface styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
 import InstitutionalApp from './InstitutionalApp.jsx';
 import './institutional.css';
+import './unified.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
