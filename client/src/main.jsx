0c0102379c8b090c31acd07ef37e8c77d6dfe295
gaurakshay1991 <gaur.akshay1991@gmail.com>
2026-07-20 12:54:32 +0530

    Activate unified Decision OS and institutional lab

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
-import InstitutionalApp from './InstitutionalApp.jsx';
+import RootApp from './RootApp.jsx';
 import './institutional.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <InstitutionalApp />
+    <RootApp />
     <Analytics />
   </React.StrictMode>
 );
