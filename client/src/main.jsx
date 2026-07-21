76b00cf50e8825bc5257d2c8329d7fd0bac15342
gaurakshay1991 <gaur.akshay1991@gmail.com>
2026-07-21 12:28:04 +0530

    Activate Synesis New Model interface

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
-import InstitutionalApp from './InstitutionalApp.jsx';
-import './institutional.css';
+import NewModelApp from './NewModelApp.jsx';
+import './new-model.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <InstitutionalApp />
+    <NewModelApp />
     <Analytics />
   </React.StrictMode>
 );
