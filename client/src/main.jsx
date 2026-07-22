20f07e979da22834ada7572c9fd6c0870bf406d0
gaurakshay1991 <gaur.akshay1991@gmail.com>
2026-07-22 22:22:52 +0530

    Load live review brain styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,8 @@ import RootApp from './RootApp.jsx';
 import './institutional.css';
 import './lab.css';
 import './themis.css';
+import './themis-suite.css';
+import './live-review-brain.css';
 import './regulatory-command.css';
 import './platform-home.css';
 
