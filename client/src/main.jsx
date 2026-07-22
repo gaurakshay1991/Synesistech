ae62dc7fd7f92542da05a14509232cef55a05e91
gaurakshay1991 <gaur.akshay1991@gmail.com>
2026-07-23 03:04:48 +0530

    Remove Vercel-specific runtime dependency

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Analytics } from '@vercel/analytics/react';
 import RootApp from './RootApp.jsx';
 import './institutional.css';
 import './lab.css';
@@ -13,6 +12,5 @@ import './platform-home.css';
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RootApp />
-    <Analytics />
   </React.StrictMode>
 );
