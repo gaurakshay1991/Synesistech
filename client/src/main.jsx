20697e75f56ec882c959212611e7e739b4c071b9
gaurakshay1991 <gaur.akshay1991@gmail.com>
2026-07-22 17:58:04 +0530

    Make PARMA the independent product entry point

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
-import RootApp from './RootApp.jsx';
-import './institutional.css';
-import './lab.css';
-import './themis.css';
-import './regulatory-command.css';
-import './platform-home.css';
+import ParmaApp from './ParmaApp.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RootApp />
+    <ParmaApp />
     <Analytics />
   </React.StrictMode>
 );
